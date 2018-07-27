4ee83e03e22b015356b064173beec899595d30af
Rebecca Tay <rebeccatay92@gmail.com>
2018-07-28 00:04:35 +0800

    go get remote package

--- hello.go
+++ hello.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-  "fmt"
-  "github.com/rebeccatay92/stringutil"
+	"fmt"
+	// "github.com/rebeccatay92/stringutil"
+	"github.com/golang/example/stringutil"
 )
 
 func main() {
 	fmt.Printf("Hello, world.\n")
-  fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
 }
